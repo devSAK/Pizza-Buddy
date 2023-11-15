d6792bc1ba016eb9b68e11a54253d3ff7b42e76d
Sabir Ahammed Khan <khansabirahammed@gmail.com>
2023-11-15 21:02:24 +0530

    Added OrderScreen Component

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -26,6 +26,8 @@ import ProfileScreen from "./screens/ProfileScreen";
 import ShippingScreen from "./screens/ShippingScreen";
 import PaymentScreen from "./screens/PaymentScreen";
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
+import OrderScreen from './screens/OrderScreen';
+
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -45,6 +47,7 @@ const router = createBrowserRouter(
         <Route path="/shipping" element={<ShippingScreen />} />
         <Route path="/payment" element={<PaymentScreen />} />
         <Route path='/placeorder' element={<PlaceOrderScreen />} />
+        <Route path='/order/:id' element={<OrderScreen />} />
         <Route path="/profile" element={<ProfileScreen />} />
       </Route>
     </Route>
