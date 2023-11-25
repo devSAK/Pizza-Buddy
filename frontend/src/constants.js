b65758766ba13efd88cf4efc7995e986d3606106
Sabir Ahammed Khan <khansabirahammed@gmail.com>
2023-11-26 02:09:25 +0530

    Delete frontend directory

--- frontend/src/constants.js
+++
@@ -1,5 +0,0 @@
-export const BASE_URL = ""; // If using proxy
-export const PRODUCTS_URL = "/api/products";
-export const USERS_URL = "/api/users";
-export const ORDERS_URL = "/api/orders";
-export const PAYPAL_URL = "/api/config/paypal";
